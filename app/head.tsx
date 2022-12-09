191b028758f811e9fe8e0676ed57193641e3eb1e
derekprieur <DPrieur123@gmail.com>
2022-12-09 08:16:36 -0500

    final

---
+++ app/head.tsx
@@ -0,0 +1,9 @@
+export default function Head() {
+  return (
+    <>
+      <title>Ninja News Squad</title>
+      <meta content="width=device-width, initial-scale=1" name="viewport" />
+      <link rel="icon" href="/favicon.ico" />
+    </>
+  )
+}
