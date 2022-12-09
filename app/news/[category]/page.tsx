2efc8ae832ef0e073d5823236ef82f01fd028050
derekprieur <DPrieur123@gmail.com>
2022-12-09 08:25:59 -0500

    final

--- app/news/[category]/page.tsx
+++ app/news/[category]/page.tsx
@@ -18,9 +18,9 @@ async function NewsCategory({params: {category}}: Props) {
 
 export default NewsCategory
 
-export async function generateStaticParams() {
-return categories.map(category => ({
-  category: category,
-}))
-}
+// export async function generateStaticParams() {
+// return categories.map(category => ({
+//   category: category,
+// }))
+// }
 
